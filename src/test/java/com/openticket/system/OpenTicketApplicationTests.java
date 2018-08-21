8b529c38f42e6572bd7237779e511429f3f6c919
jszlenk <jakubszlenk@fastmail.com>
2018-08-21 13:39:56 +0200

    Open Ticket App

--- src/test/java/com/openticket/system/OpenTicketApplicationTests.java
+++ src/test/java/com/openticket/system/OpenTicketApplicationTests.java
@@ -9,8 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class OpenTicketApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
-
+    @Test
+    public void contextLoads() {
+    }
 }
